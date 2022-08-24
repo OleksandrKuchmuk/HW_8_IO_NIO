819df2875a21f3d4afebf966e2dcb1ea0af7bbd5
Oleksandr Kuchmuk <96177236+OleksandrKuchmuk@users.noreply.github.com>
2022-08-24 18:52:35 +0300

    Initial Commit. Start work

---
+++ src/main/java/com/cursor/Task1/Main_Task1.java
@@ -0,0 +1,12 @@
+package com.cursor.Task1;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main_Task1 {
+    public static final Logger LOGGER = LogManager.getLogger(Main_Task1.class);
+
+    public static void main(String[] args) {
+
+    }
+}
